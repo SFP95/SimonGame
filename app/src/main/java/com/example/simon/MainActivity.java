36aebcfda4f37e3da45961a4d9154b7791dfb4c1
nia <dam.sonia.fernandez@gmail.com>
2023-05-29 23:05:28 +0200

    xml view

--- app/src/main/java/com/example/simon/MainActivity.java
+++ app/src/main/java/com/example/simon/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simon;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnComenzar;
     private Button btnConfigurar;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
