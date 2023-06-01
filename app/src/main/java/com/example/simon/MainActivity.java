d745fe86a31aec8fe9da81c0d07a75b6c6b128d0
nia <dam.sonia.fernandez@gmail.com>
2023-06-01 21:49:38 +0200

    codigo

--- app/src/main/java/com/example/simon/MainActivity.java
+++ app/src/main/java/com/example/simon/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.simon;
 
-import static android.view.View.INVISIBLE;
-import static android.view.View.VISIBLE;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,12 +12,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private Button btnComenzar, btnConfigurar;
     private Button btnRojo, btnVerde, btnAmarillo, btnAzul;
     private TextView tvTime, tvScore, tvHiScore;
-    private int colores[]={Color.RED, Color.BLUE,Color.YELLOW,Color.GREEN};
-    private int index=0;
+
+    private List<Integer> secuenciaAleatoria;
+    private List<Integer> secuenciaUsuario;
+    private int puntuacion;
+    private int tiempoJuego = 60000;
+    private int tiempoPersistenciaColor = 500;
+    private int tiempoEsperaColor = 50;
+    private int puntuacionMaxima;
+    private boolean sonidosActivados;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,116 +36,241 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        asignartvs();
-        asignarBotonesFuncioens();
+        asignarVistas();
+        asignarBotonesFunciones();
         asignarBotonesColores();
         inhabilitarBotonesColores();
-    }
-    public void onComenzar(View view) {
-        habilitarBotonesColores();
-        reproducirColoresRandom();
+
+        // Cargar la puntuación máxima guardada
+        puntuacionMaxima = obtenerPuntuacionMaxima();
+
+        // Configurar el juego inicialmente
+        configurarJuego();
     }
 
-    private void reproducirColoresRandom() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    for (int i = 0; i < colores.length; i++) {
-                        Thread.sleep(1000);
-                        mostrarColor(colores[i]);
-                        Thread.sleep(8000);
-                        ocultarColores();
-                        Thread.sleep(1000);
-                        habilitarBotonesColores();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "TU TURNO", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-        thread.start();
-    }
-    private void mostrarColor(final int color) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                switch (color) {
-                    case Color.RED:
-                        btnRojo.setBackgroundColor(color);
-                        break;
-                    case Color.BLUE:
-                        btnAzul.setBackgroundColor(color);
-                        break;
-                    case Color.YELLOW:
-                        btnAmarillo.setBackgroundColor(color);
-                        break;
-                    case Color.GREEN:
-                        btnVerde.setBackgroundColor(color);
-                        break;
-                }
-            }
-        });
-    }
-    private void ocultarColores() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                btnAmarillo.setVisibility(INVISIBLE);
-                btnAzul.setVisibility(INVISIBLE);
-                btnVerde.setVisibility(INVISIBLE);
-                btnRojo.setVisibility(INVISIBLE);
-            }
-        });
-    }
-    public void onConfigurar(View view) {
-    }
-    public void habilitarBotonesColores(){
-        btnAmarillo.setVisibility(VISIBLE);
-        btnAzul.setVisibility(VISIBLE);
-        btnVerde.setVisibility(VISIBLE);
-        btnRojo.setVisibility(VISIBLE);
-    }
-    public void inhabilitarBotonesColores(){
-        btnAmarillo.setVisibility(INVISIBLE);
-        btnAzul.setVisibility(INVISIBLE);
-        btnVerde.setVisibility(INVISIBLE);
-        btnRojo.setVisibility(INVISIBLE);
-    }
-    public void asignarBotonesFuncioens() {
+    private void asignarVistas() {
         btnComenzar = findViewById(R.id.btnComenzar);
         btnConfigurar = findViewById(R.id.btnConfigurar);
-    }
-    public void asignarBotonesColores() {
         btnRojo = findViewById(R.id.btnRojo);
         btnAmarillo = findViewById(R.id.btnAmarillo);
         btnAzul = findViewById(R.id.btnAzul);
         btnVerde = findViewById(R.id.btnVerde);
-    }
-    public void asignartvs(){
-        tvScore=findViewById(R.id.tvScore);
-        tvHiScore=findViewById(R.id.tvhiScore);
-        tvTime=findViewById(R.id.tvTime);
+        tvTime = findViewById(R.id.tvTime);
+        tvScore = findViewById(R.id.tvScore);
+        tvHiScore = findViewById(R.id.tvhiScore);
     }
 
-    public void onRojo(View view) {
+    private void asignarBotonesFunciones() {
+        btnComenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJuego();
+            }
+        });
+
+        btnConfigurar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir la actividad de configuración
+                abrirActividadConfiguracion();
+            }
+        });
     }
 
-    public void onAma(View view) {
+    private void asignarBotonesColores() {
+        btnRojo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarRespuesta(Color.RED);
+            }
+        });
+
+        btnAmarillo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarRespuesta(Color.YELLOW);
+            }
+        });
+
+        btnAzul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarRespuesta(Color.BLUE);
+            }
+        });
+
+        btnVerde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarRespuesta(Color.GREEN);
+            }
+        });
     }
 
-    public void onVerde(View view) {
+    private void inhabilitarBotonesColores() {
+        btnAmarillo.setVisibility(View.INVISIBLE);
+        btnAzul.setVisibility(View.INVISIBLE);
+        btnVerde.setVisibility(View.INVISIBLE);
+        btnRojo.setVisibility(View.INVISIBLE);
     }
 
-    public void onAzul(View view) {
+    private void habilitarBotonesColores() {
+        btnAmarillo.setVisibility(View.VISIBLE);
+        btnAzul.setVisibility(View.VISIBLE);
+        btnVerde.setVisibility(View.VISIBLE);
+        btnRojo.setVisibility(View.VISIBLE);
     }
 
+    private void iniciarJuego() {
+        // Reiniciar la puntuación y las secuencias
+        puntuacion = 0;
+        secuenciaAleatoria = new ArrayList<>();
+        secuenciaUsuario = new ArrayList<>();
 
+        // Generar la primera secuencia aleatoria
+        generarSecuenciaAleatoria();
+
+        // Iniciar la reproducción de la secuencia
+        reproducirSecuencia();
+
+        // Mostrar el mensaje "TU TURNO" y habilitar los botones de colores
+        Toast.makeText(MainActivity.this, "TU TURNO", Toast.LENGTH_SHORT).show();
+        habilitarBotonesColores();
+    }
+
+    private void generarSecuenciaAleatoria() {
+        Random random = new Random();
+
+        for (int i = 0; i < 4; i++) {
+            int color = generarColorAleatorio();
+            secuenciaAleatoria.add(color);
+        }
+    }
+
+    private int generarColorAleatorio() {
+        int[] colores = {Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN};
+        Random random = new Random();
+        int indice = random.nextInt(colores.length);
+        return colores[indice];
+    }
+
+    private void reproducirSecuencia() {
+        new CountDownTimer(secuenciaAleatoria.size() * (tiempoPersistenciaColor + tiempoEsperaColor), tiempoPersistenciaColor + tiempoEsperaColor) {
+            int indiceColor = 0;
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mostrarColor(secuenciaAleatoria.get(indiceColor));
+                indiceColor++;
+            }
+
+            @Override
+            public void onFinish() {
+                ocultarColores();
+
+                // Mostrar mensaje y habilitar los botones para que el usuario responda
+                Toast.makeText(MainActivity.this, "TU TURNO", Toast.LENGTH_SHORT).show();
+                habilitarBotonesColores();
+            }
+        }.start();
+    }
+
+    private void mostrarColor(int color) {
+        // Mostrar el color cambiando el fondo del botón correspondiente
+        if (color == Color.RED) {
+            btnRojo.setBackgroundColor(color);
+        } else if (color == Color.YELLOW) {
+            btnAmarillo.setBackgroundColor(color);
+        } else if (color == Color.BLUE) {
+            btnAzul.setBackgroundColor(color);
+        } else if (color == Color.GREEN) {
+            btnVerde.setBackgroundColor(color);
+        }
+    }
+
+    private void ocultarColores() {
+        // Restaurar el fondo de los botones a su color original
+        btnRojo.setBackgroundColor(Color.LTGRAY);
+        btnAmarillo.setBackgroundColor(Color.LTGRAY);
+        btnAzul.setBackgroundColor(Color.LTGRAY);
+        btnVerde.setBackgroundColor(Color.LTGRAY);
+    }
+
+    private void registrarRespuesta(int color) {
+        secuenciaUsuario.add(color);
+
+        // Comprobar si se ha completado la secuencia del usuario
+        if (secuenciaUsuario.size() == secuenciaAleatoria.size()) {
+            compararSecuencias();
+        }
+    }
+
+    private void compararSecuencias() {
+        boolean secuenciasIguales = true;
+
+        for (int i = 0; i < secuenciaAleatoria.size(); i++) {
+            if (!secuenciaAleatoria.get(i).equals(secuenciaUsuario.get(i))) {
+                secuenciasIguales = false;
+                break;
+            }
+        }
+
+        if (secuenciasIguales) {
+            // Las secuencias son iguales
+            Toast.makeText(MainActivity.this, "MUY BIEN, probemos ahora con 5", Toast.LENGTH_SHORT).show();
+            puntuacion += Math.round(secuenciaAleatoria.size() * 0.7);
+
+            // Generar la siguiente secuencia aleatoria
+            generarSecuenciaAleatoria();
+
+            // Reiniciar la secuencia del usuario
+            secuenciaUsuario.clear();
+
+            // Iniciar la reproducción de la siguiente secuencia
+            reproducirSecuencia();
+        } else {
+            // Las secuencias no son iguales
+            Toast.makeText(MainActivity.this, "Secuencia incorrecta. Vuelve a intentarlo.", Toast.LENGTH_SHORT).show();
+
+            // Reproducir la última secuencia aleatoria no acertada
+            reproducirUltimaSecuencia();
+        }
+    }
+
+    private void reproducirUltimaSecuencia() {
+        new CountDownTimer(secuenciaAleatoria.size() * (tiempoPersistenciaColor + tiempoEsperaColor), tiempoPersistenciaColor + tiempoEsperaColor) {
+            int indiceColor = 0;
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mostrarColor(secuenciaAleatoria.get(indiceColor));
+                indiceColor++;
+            }
+
+            @Override
+            public void onFinish() {
+                ocultarColores();
+
+                // Mostrar mensaje y habilitar los botones para que el usuario responda
+                Toast.makeText(MainActivity.this, "TU TURNO", Toast.LENGTH_SHORT).show();
+                habilitarBotonesColores();
+            }
+        }.start();
+    }
+
+    private void abrirActividadConfiguracion() {
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+
+    private void configurarJuego() {
+        // Configurar el juego inicialmente
+        // ...
+    }
+
+    private int obtenerPuntuacionMaxima() {
+        // Obtener la puntuación máxima guardada
+        // ...
+        return 0;
+    }
 }
